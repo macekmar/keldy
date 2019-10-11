cd17cd4c54a4be8fdcbb548679bdb2d370fe4b7f
Philipp D <pdumitrescu@flatironinstitute.org>
2019-10-11 09:36:08 -0400

    Removes unneeded warper eraser

--- c++/keldy/warpers/warpers.hpp
+++ c++/keldy/warpers/warpers.hpp
@@ -32,28 +32,5 @@
 
 namespace keldy {
 
-// // warper eraser
-// class warper_t {
-//  private:
-//   std::any warp;
-//   std::function<std::vector<double>(std::vector<double> const &)> ui_from_li_;
-//   std::function<double(std::vector<double> const &)> jacobian_;
-
-//  public:
-
-//   warper_t() = default;
-
-//   template <typename T>
-//   CPP2PY_IGNORE warper_t(T x) : warp{std::move(x)} {
-//     T const *p = std::any_cast<T const>(&warp); // retrieve a pointer on the stored T object
-
-//     ui_from_li_ = [p](std::vector<double> const &li_vec) { return p->ui_from_li(li_vec); };
-//     jacobian_  = [p](std::vector<double> const &li_vec) { return p->jacobian(li_vec); };
-//   }
-
-//   double jacobian(std::vector<double> const &x) const {return jacobian_(x);}
-//   std::vector<double> ui_from_li(std::vector<double> const &x) const {return ui_from_li_(x);}
-
-// };
 
 } // namespace keldy
