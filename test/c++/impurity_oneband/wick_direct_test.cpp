2f4f608a65ceaa9921eee2a660bcbefaf432dc04
Corentin Bertrand <cbertrand@flatironinstitute.org>
2019-10-18 18:37:59 -0400

    cleanup of wick direct test

--- test/c++/impurity_oneband/wick_direct_test.cpp
+++ test/c++/impurity_oneband/wick_direct_test.cpp
@@ -5,6 +5,7 @@
 using namespace keldy;
 using namespace keldy::impurity_oneband;
 
+<<<<<<< HEAD
 //dcomplex wick_det(g0_keldysh_contour_t& g0_k, std::vector<gf_index_t>& list_rows, std::vector<gf_index_t>& list_cols) {
 //  size_t N = list_rows.size();
 //  if (N != list_cols.size())
@@ -31,6 +32,9 @@ TEST(integrand_direct, Initialize) { // NOLINT
 }
 
 TEST(integrand_direct, Values) { // NOLINT
+=======
+TEST(integrand_direct, Order_1) { // NOLINT
+>>>>>>> 960b3a6... cleanup of wick direct test
   model_param_t params;
   g0_model g0{params};
   g0_keldysh_contour_t g0_k{g0};
@@ -40,69 +44,78 @@ TEST(integrand_direct, Values) { // NOLINT
 
   dcomplex computed_val, expected_val;
 
-  // Order 1 ignoring disconnected diagrams
+  // We ignore disconnected diagrams
   expected_val = 0;
+
   for (keldysh_idx_t a : {forward, backward}) {
     int sign = (a == 0) ? 1 : -1;
-    expected_val += - sign * g0_k({1.0, down, forward}, {1.0, down, backward})
-       * g0_k(external_A, {1.0, up, a}) * g0_k({1.0, up, a}, external_B);
+    expected_val += -sign * g0_k({1.0, down, forward}, {1.0, down, backward}) * g0_k(external_A, {1.0, up, a})
+       * g0_k({1.0, up, a}, external_B);
   }
-  std::cout << expected_val << std::endl;
 
   computed_val = integrand(std::vector<double>{1.0});
-  std::cout << computed_val << std::endl;
 
-  EXPECT_DOUBLE_EQ(std::real(expected_val), std::real(computed_val));
-  EXPECT_NEAR(std::imag(expected_val), std::imag(computed_val), 1e-17); // absolute tolerance because we compute 0
+  EXPECT_COMPLEX_NEAR(expected_val, computed_val, 1e-17);
+}
 
-  // Order 2 with full determinants (unprecise)
-  gf_index_t A, B;
-  dcomplex det_up, det_down;
+TEST(integrand_direct, Order_2) { // NOLINT
+  model_param_t params;
+  g0_model g0{params};
+  g0_keldysh_contour_t g0_k{g0};
+  auto external_A = gf_index_t{5.0, up, forward};
+  auto external_B = gf_index_t{5.0, up, forward};
+  integrand_g_t1t2_direct integrand{g0_k, external_A, external_B};
+
+  dcomplex computed_val, expected_val;
+
+  //// Order 2 with full determinants (unprecise)
+  //gf_index_t A, B;
+  //dcomplex det_up, det_down;
+  //expected_val = 0;
+
+  //for (keldysh_idx_t a : {forward, backward}) {
+  //  for (keldysh_idx_t b : {forward, backward}) {
+  //    int sign = (((a + b) % 2) == 0) ? 1 : -1;
+  //    A = {1.0, up, a};
+  //    B = {2.0, up, b};
+  //    det_up =
+  //      - (g0_k({1.0, up, forward}, {1.0, up, backward}) * g0_k(external_A, B) - g0_k(external_A, A) * g0_k(A, B)) * g0_k(B, external_B)
+  //      + (g0_k(B, A) * g0_k(external_A, B) - g0_k(external_A, A) * g0_k({2.0, up, forward}, {2.0, up, backward})) * g0_k(A, external_B);
+  //    A = {1.0, down, a};
+  //    B = {2.0, down, b};
+  //    det_down = g0_k({1.0, down, forward}, {1.0, up, backward}) * g0_k({2.0, down, forward}, {2.0, up, backward})
+  //      - g0_k(B, A) * g0_k(A, B);
+  //    expected_val += sign * det_up * det_down;
+  //  }
+  //}
+
+  //computed_val = integrand(std::vector<double>{1.0, 2.0});
+
+  //EXPECT_COMPLEX_NEAR(expected_val, computed_val, 1e-16); // imag part very close to 0
+
+  // We ignore disconnected diagrams
   expected_val = 0;
+
   for (keldysh_idx_t a : {forward, backward}) {
     for (keldysh_idx_t b : {forward, backward}) {
       int sign = (((a + b) % 2) == 0) ? 1 : -1;
-      A = {1.0, up, a};
-      B = {2.0, up, b};
-      det_up =
-        - (g0_k({1.0, up, forward}, {1.0, up, backward}) * g0_k(external_A, B) - g0_k(external_A, A) * g0_k(A, B)) * g0_k(B, external_B)
-        + (g0_k(B, A) * g0_k(external_A, B) - g0_k(external_A, A) * g0_k({2.0, up, forward}, {2.0, up, backward})) * g0_k(A, external_B);
-      A = {1.0, down, a};
-      B = {2.0, down, b};
-      det_down = g0_k({1.0, down, forward}, {1.0, up, backward}) * g0_k({2.0, down, forward}, {2.0, up, backward})
-        - g0_k(B, A) * g0_k(A, B);
-      expected_val += sign * det_up * det_down;
-    }
-  }
-  std::cout << expected_val << std::endl;
-
-  computed_val = integrand(std::vector<double>{1.0, 2.0});
-  std::cout << computed_val << std::endl;
-
-  EXPECT_NEAR(std::real(expected_val), std::real(computed_val), 1e-16); // absolute tolerance because we compute 0
-  EXPECT_DOUBLE_EQ(std::imag(expected_val), std::imag(computed_val));
-
-  // Order 2 ignoring disconnected diagrams
-  expected_val = 0;
-  for (keldysh_idx_t a : {forward, backward}) {
-    for (keldysh_idx_t b : {forward, backward}) {
-      int sign = (((a + b) % 2) == 0) ? 1 : -1;
-      expected_val += sign * (g0_k({1.0, up, forward}, {1.0, up, backward}) * g0_k(external_A, {2.0, up, b}) * g0_k({2.0, up, b}, external_B)
-          + g0_k(external_A, {1.0, up, a}) * g0_k({2.0, up, forward}, {2.0, up, backward}) * g0_k({1.0, up, a}, external_B))
-        * g0_k({2.0, down, b}, {1.0, down, a}) * g0_k({1.0, down, a}, {2.0, down, b});
-      expected_val += sign * (g0_k(external_A, {1.0, up, a}) * g0_k({1.0, up, a}, {2.0, up, b}) * g0_k({2.0, up, b}, external_B)
-          + g0_k({2.0, up, b}, {1.0, up, a}) * g0_k(external_A, {2.0, up, b}) * g0_k({1.0, up, a}, external_B))
-        * (g0_k({1.0, down, forward}, {1.0, down, backward}) * g0_k({2.0, down, forward}, {2.0, down, backward})
+      expected_val += sign
+         * (g0_k({1.0, up, forward}, {1.0, up, backward}) * g0_k(external_A, {2.0, up, b})
+               * g0_k({2.0, up, b}, external_B)
+            + g0_k(external_A, {1.0, up, a}) * g0_k({2.0, up, forward}, {2.0, up, backward})
+               * g0_k({1.0, up, a}, external_B))
+         * g0_k({2.0, down, b}, {1.0, down, a}) * g0_k({1.0, down, a}, {2.0, down, b});
+      expected_val += sign
+         * (g0_k(external_A, {1.0, up, a}) * g0_k({1.0, up, a}, {2.0, up, b}) * g0_k({2.0, up, b}, external_B)
+            + g0_k({2.0, up, b}, {1.0, up, a}) * g0_k(external_A, {2.0, up, b}) * g0_k({1.0, up, a}, external_B))
+         * (g0_k({1.0, down, forward}, {1.0, down, backward}) * g0_k({2.0, down, forward}, {2.0, down, backward})
             - g0_k({2.0, down, b}, {1.0, down, a}) * g0_k({1.0, down, a}, {2.0, down, b}));
     }
   }
-  std::cout << expected_val << std::endl;
 
   computed_val = integrand(std::vector<double>{1.0, 2.0});
-  std::cout << computed_val << std::endl;
 
-  EXPECT_NEAR(std::real(expected_val), std::real(computed_val), 1e-16); // absolute tolerance because we compute 0
-  EXPECT_DOUBLE_EQ(std::imag(expected_val), std::imag(computed_val));
+  EXPECT_COMPLEX_NEAR(expected_val, computed_val, 1e-16); // real part very close to 0
 }
 
 MAKE_MAIN; // NOLINT
