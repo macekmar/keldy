c0d30530c72240c04381fbf1b1fe366b4ea43a76
Philipp D <pdumitrescu@flatironinstitute.org>
2019-10-21 09:59:01 -0400

    cleanup kernel implementation

--- c++/keldy/impurity_oneband/wick_kernel.cpp
+++ c++/keldy/impurity_oneband/wick_kernel.cpp
@@ -1,156 +1,166 @@
-// // //******************************************************************************
-// // //
-// // // keldy
-// // //
-// // // Copyright (C) 2019, The Simons Foundation
-// // // authors: Philipp Dumitrescu
-// // //
-// // // keldy is free software: you can redistribute it and/or modify it under the
-// // // terms of the GNU General Public License as published by the Free Software
-// // // Foundation, either version 3 of the License, or (at your option) any later
-// // // version.
-// // //
-// // // keldy is distributed in the hope that it will be useful, but WITHOUT ANY
-// // // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// // // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// // // details.
-// // //
-// // // You should have received a copy of the GNU General Public License along with
-// // // keldy. If not, see <http://www.gnu.org/licenses/>.
-// // //
-// // //******************************************************************************
+// //******************************************************************************
+// //
+// // keldy
+// //
+// // Copyright (C) 2019, The Simons Foundation
+// // authors: Philipp Dumitrescu
+// //
+// // keldy is free software: you can redistribute it and/or modify it under the
+// // terms of the GNU General Public License as published by the Free Software
+// // Foundation, either version 3 of the License, or (at your option) any later
+// // version.
+// //
+// // keldy is distributed in the hope that it will be useful, but WITHOUT ANY
+// // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// // details.
+// //
+// // You should have received a copy of the GNU General Public License along with
+// // keldy. If not, see <http://www.gnu.org/licenses/>.
+// //
+// //******************************************************************************
 
-// #include "wick_kernel.hpp"
+#include "wick_kernel.hpp"
+#include <triqs/utility/exceptions.hpp>
+#include <triqs/arrays.hpp>
+#include <triqs/arrays/matrix.hpp>
 
-// namespace {
+#include <triqs/arrays/blas_lapack/tools.hpp>
+#include <triqs/arrays/blas_lapack/getrs.hpp>
 
-// inline int GetBit(int in, int offset) { return (in & (1 << offset)) != 0; }
+namespace {
 
-// inline int GetBitParity(unsigned int in) { return 1 - 2 * __builtin_parity(in); }
+inline int GetBit(int in, int offset) { return (in & (1 << offset)) != 0; }
 
-// } // namespace
+inline int GetBitParity(unsigned int in) { return 1 - 2 * __builtin_parity(in); }
 
-// namespace keldy::impurity_oneband {
+} // namespace
 
-// // TODO: Where is sorting of times done?
-// // TODO: Efficnencies for spin symmetric case.
+namespace keldy::impurity_oneband {
 
-// std::vector<std::pair<gf_index_t, dcomplex>> integrand_g_kernel::operator()(std::vector<double> const &times) const {
-//   using namespace triqs::arrays;
-//   // Interaction starts a t = 0
-//   if (*std::min_element(times.begin(), times.end()) < 0) { // can replace with a for_any
-//     return std::vector<std::pair<gf_index_t, dcomplex>>{};
-//   }
+// TODO: Where is sorting of times done?
+// TODO: Efficnencies for spin symmetric case.
 
-//   int order_n = times.size();
+sparse_kernel_binner integrand_g_kernel::operator()(std::vector<double> const &times) const {
+  using namespace triqs::arrays;
+  // Interaction starts a t = 0
+  if (*std::min_element(times.begin(), times.end()) < 0) { // can replace with a for_any
+    return sparse_kernel_binner{};
+  }
 
-//   // if (order_n == 0) {
-//   //   return 1.0; // TODO: CHECK NORMALIZATION
-//   // }
+  int order_n = times.size();
 
-//   auto a = g_idx_X; // This is now a dummy index
-//   auto b = g_idx_X;
-//   // define time-splitting for external-points
-//   a.timesplit_n = order_n;
-//   b.timesplit_n = order_n;
+  // if (order_n == 0) {
+  //   return 1.0; // TODO: CHECK NORMALIZATION
+  // }
 
-//   // Pre-Comute Large Matrix.
-//   // "s1": Same spin as external indices / "s2": Opposite spin
-//   matrix<dcomplex> wick_matrix_s1(2 * order_n + 1, 2 * order_n + 1);
-//   matrix<dcomplex> wick_matrix_s2(2 * order_n, 2 * order_n);
+  sparse_kernel_binner result{};
 
-//   // Vector of indices for Green functions
-//   std::vector<gf_index_t> all_config_1(2 * order_n);
-//   std::vector<gf_index_t> all_config_2(2 * order_n);
-//   for (int i = 0; i < order_n; i++) {
-//     all_config_1[i] = gf_index_t{times[i], a.spin, forward, i};
-//     all_config_1[i + order_n] = gf_index_t{times[i], a.spin, backward, i};
-//     all_config_2[i] = gf_index_t{times[i], spin_t(1 - a.spin), forward, i};
-//     all_config_2[i + order_n] = gf_index_t{times[i], spin_t(1 - a.spin), backward, i};
-//   }
+  auto a = g_idx_X; // This is now a dummy index
+  auto b = g_idx_X;
+  // define time-splitting for external-points
+  a.timesplit_n = order_n;
+  b.timesplit_n = order_n;
 
-//   // Index for external index in s1
-//   int external_idx = 2 * order_n;
+  // Pre-Comute Large Matrix.
+  // "s1": Same spin as external indices / "s2": Opposite spin
+  matrix<dcomplex> wick_matrix_s1(2 * order_n + 1, 2 * order_n + 1);
+  matrix<dcomplex> wick_matrix_s2(2 * order_n, 2 * order_n);
 
-//   wick_matrix_s1(external_idx, external_idx) = g0(a, b, false);
-//   // #pragma omp parallel for
-//   for (int i = 0; i < 2 * order_n; i++) {
-//     wick_matrix_s1(external_idx, i) = g0(a, all_config_1[i]);
-//     wick_matrix_s1(i, external_idx) = g0(all_config_1[i], b);
-//     for (int j = 0; j < 2 * order_n; j++) {
-//       wick_matrix_s1(i, j) = g0(all_config_1[i], all_config_1[j]);
-//       wick_matrix_s2(i, j) = g0(all_config_2[i], all_config_2[j]);
-//     }
-//   }
+  // Vector of indices for Green functions
+  std::vector<gf_index_t> all_config_1(2 * order_n);
+  std::vector<gf_index_t> all_config_2(2 * order_n);
+  for (int i = 0; i < order_n; i++) {
+    all_config_1[i] = gf_index_t{times[i], a.spin, forward, i};
+    all_config_1[i + order_n] = gf_index_t{times[i], a.spin, backward, i};
+    all_config_2[i] = gf_index_t{times[i], spin_t(1 - a.spin), forward, i};
+    all_config_2[i + order_n] = gf_index_t{times[i], spin_t(1 - a.spin), backward, i};
+  }
 
-//   dcomplex integrand_result = 0.0;
-//   uint64_t nr_keldysh_configs = (uint64_t(1) << order_n);
+  // Index for external index in s1
+  int external_idx = 2 * order_n;
 
-//   // Iterate over other Keldysh index configurations. Splict smaller determinant from precomuted matrix
-//   // #pragma omp parallel for reduce(+: integrand_result)
-//   for (uint64_t idx_kel = 0; idx_kel < nr_keldysh_configs - 1; idx_kel++) {
-//     // Indices of Rows / Cols to pick. Cycle through and shift by (0/1) * order_n depending on idx_kel configuration
-//     std::vector<int> col_pick_s1(order_n + 1);
-//     std::vector<int> col_pick_s2(order_n);
+  wick_matrix_s1(external_idx, external_idx) = g0(a, b, false);
+  // #pragma omp parallel for
+  for (int i = 0; i < 2 * order_n; i++) {
+    wick_matrix_s1(external_idx, i) = g0(a, all_config_1[i]);
+    wick_matrix_s1(i, external_idx) = g0(all_config_1[i], b);
+    for (int j = 0; j < 2 * order_n; j++) {
+      wick_matrix_s1(i, j) = g0(all_config_1[i], all_config_1[j]);
+      wick_matrix_s2(i, j) = g0(all_config_2[i], all_config_2[j]);
+    }
+  }
 
-//     col_pick_s1[0] = external_idx;
-//     for (int i = 0; i < order_n; i++) {
-//       col_pick_s1[i + 1] = i + GetBit(idx_kel, i) * order_n;
-//       col_pick_s2[i] = col_pick_s1[i + 1];
-//     }
+  dcomplex integrand_result = 0.0;
+  uint64_t nr_keldysh_configs = (uint64_t(1) << order_n);
 
-//     // Extract data into temporary matrices
-//     matrix<dcomplex> g_mat_s1(order_n + 1, order_n + 1, FORTRAN_LAYOUT);
-//     matrix<dcomplex> g_mat_s2(order_n, order_n, FORTRAN_LAYOUT);
+  // Iterate over other Keldysh index configurations. Splict smaller determinant from precomuted matrix
+  // #pragma omp parallel for reduce(+: integrand_result)
+  for (uint64_t idx_kel = 0; idx_kel < nr_keldysh_configs - 1; idx_kel++) {
+    // Indices of Rows / Cols to pick. Cycle through and shift by (0/1) * order_n depending on idx_kel configuration
+    std::vector<int> col_pick_s1(order_n + 1);
+    std::vector<int> col_pick_s2(order_n);
 
-//     for (auto [i, j] : itertools::zip(col_pick_s2, col_pick_s2)) {
-//       g_mat_s2(i, j) = wick_matrix_s2(col_pick_s2[i], col_pick_s2[j]);
-//     }
+    col_pick_s1[0] = external_idx;
+    for (int i = 0; i < order_n; i++) {
+      col_pick_s1[i + 1] = i + GetBit(idx_kel, i) * order_n;
+      col_pick_s2[i] = col_pick_s1[i + 1];
+    }
 
-//     for (auto [i, j] : itertools::zip(col_pick_s1, col_pick_s1)) {
-//       g_mat_s1(i, j) = wick_matrix_s1(col_pick_s1[i], col_pick_s1[j]);
-//     }
+    // Extract data into temporary matrices
+    matrix<dcomplex> g_mat_s1(order_n + 1, order_n + 1, FORTRAN_LAYOUT);
+    matrix<dcomplex> g_mat_s2(order_n, order_n, FORTRAN_LAYOUT);
 
-//     // Strategy: Do row expansion by solving linear equation for cofactors.
-//     // TODO: Do we want to go to full pivoting rather than partial pivoting for LU decomposition?
-//     // TODO: Consider checking Condition Number via LAPACK_zgecon
-//     // TODO: Consider ZGEEQU for equilibiration (scaling of rows / columns for better conditioning)
+    for (auto [i, j] : itertools::zip(col_pick_s2, col_pick_s2)) {
+      g_mat_s2(i, j) = wick_matrix_s2(col_pick_s2[i], col_pick_s2[j]);
+    }
 
-//     // Calculate LU decomposition with partial pivoting
-//     triqs::arrays::vector<int> pivot_index_array(first_dim(g_mat_s1));
-//     triqs::arrays::lapack::getrf(g_mat_s1, pivot_index_array, true);
-//     // == LAPACK_zgetrf(&g_mat_s1.rows(), &g_mat_s1.cols(), g_mat_s1.data(), &g_mat_s1.rows(), g_mat_s1.data(), &info);
+    for (auto [i, j] : itertools::zip(col_pick_s1, col_pick_s1)) {
+      g_mat_s1(i, j) = wick_matrix_s1(col_pick_s1[i], col_pick_s1[j]);
+    }
 
-//     // Extract det from LU decompositon (note permutations)
-//     dcomplex g_mat_s1_det = 1.0;
-//     int det_flip = 1;
-//     for (int i = 0; i < first_dim(g_mat_s1); i++) {
-//       g_mat_s1_det *= g_mat_s1(i, i);
-//       if (pivot_index_array(i) != i + 1) { // TODO: check +1 from fortran index
-//         det_flip = - det_flip;
-//       }
-//     }
-//     g_mat_s1_det *= det_flip;
+    // Strategy: Do row expansion by solving linear equation for cofactors.
+    // TODO: Do we want to go to full pivoting rather than partial pivoting for LU decomposition?
+    // TODO: Consider checking Condition Number via LAPACK_zgecon
+    // TODO: Consider ZGEEQU for equilibiration (scaling of rows / columns for better conditioning)
 
-//     // Find cofactors by solving linear equation Ax = e1 * det(A)
-//     triqs::arrays::vector<dcomplex> x_minors(first_dim(g_mat_s1));
-//     x_minors() = 0;
-//     x_minors(0) = g_mat_s1_det;
+    // Calculate LU decomposition with partial pivoting
+    triqs::arrays::vector<int> pivot_index_array(first_dim(g_mat_s1));
+    int info = triqs::arrays::lapack::getrf(g_mat_s1, pivot_index_array, true);
+    if (info != 0) {
+      TRIQS_RUNTIME_ERROR << "lapack::getrf failed with code " << info;
+    }
 
-//     // triqs::arrays::lapack::getrs(g_mat_s1, x_minors, ipiv);
-//     // LAPACK_zgetrs('N', &g_mat_s1.rows(), &nrhs, g_mat_s1.data(), &g_mat_s1.rows(), g_mat_s1.data(), x_minors.data(), &x_minors.rows(), &info);
+    // Extract det from LU decompositon (note permutations)
+    dcomplex g_mat_s1_det = 1.0;
+    int det_flip = 1;
+    for (int i = 0; i < first_dim(g_mat_s1); i++) {
+      g_mat_s1_det *= g_mat_s1(i, i);
+      if (pivot_index_array(i) != i + 1) { // TODO: check +1 from fortran index
+        det_flip = -det_flip;
+      }
+    }
+    g_mat_s1_det *= det_flip;
 
-//     // Multiply by Keldysh index parity & other spin determinant:
-//     x_minors *= GetBitParity(idx_kel) * determinant(g_mat_s2);
+    // Find cofactors by solving linear equation Ax = e1 * det(A)
+    matrix<dcomplex> x_minors(first_dim(g_mat_s1), 1, FORTRAN_LAYOUT);
+    x_minors() = 0;
+    x_minors(0, 0) = g_mat_s1_det;
 
-//     std::vector<std::pair<gf_index_t, dcomplex>> result(order_n);
+    info = triqs::arrays::lapack::getrs(g_mat_s1, x_minors, pivot_index_array);
+    if (info != 0) {
+      TRIQS_RUNTIME_ERROR << "lapack::getrs failed with code " << info;
+    }
 
-//     // Bin: Find gf_index to bin to. We leave out first element, which connects external verticies only
-//     for (int i = 1; i < x_minors.size(); i++) {
-//       result.push_back(std::make_pair(all_config_1[col_pick_s1[i]], x_minors(i)));
-//     }
-//   }
-//   return result;
-// }
+    // Multiply by Keldysh index parity & other spin determinant:
+    x_minors *= GetBitParity(idx_kel) * determinant(g_mat_s2);
 
-// } // namespace keldy::impurity_oneband
+    // Bin: Find gf_index to bin to. We leave out first element, which connects external verticies only
+    for (int i = 1; i < x_minors.size(); i++) {
+      result.data.emplace_back(std::make_pair(all_config_1[col_pick_s1[i]], x_minors(i, 0)));
+    }
+  }
+  return result;
+}
+
+} // namespace keldy::impurity_oneband
--- c++/keldy/impurity_oneband/wick_kernel.hpp
+++ c++/keldy/impurity_oneband/wick_kernel.hpp
@@ -1,135 +1,158 @@
-// //******************************************************************************
-// //
-// // keldy
-// //
-// // Copyright (C) 2019, The Simons Foundation
-// // authors: Philipp Dumitrescu
-// //
-// // keldy is free software: you can redistribute it and/or modify it under the
-// // terms of the GNU General Public License as published by the Free Software
-// // Foundation, either version 3 of the License, or (at your option) any later
-// // version.
-// //
-// // keldy is distributed in the hope that it will be useful, but WITHOUT ANY
-// // WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-// // details.
-// //
-// // You should have received a copy of the GNU General Public License along with
-// // keldy. If not, see <http://www.gnu.org/licenses/>.
-// //
-// //******************************************************************************
+//******************************************************************************
+//
+// keldy
+//
+// Copyright (C) 2019, The Simons Foundation
+// authors: Philipp Dumitrescu
+//
+// keldy is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// keldy is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// keldy. If not, see <http://www.gnu.org/licenses/>.
+//
+//******************************************************************************
 
-// #pragma once
+#pragma once
 
-// #include "../common.hpp"
-// #include "model.hpp"
+#include "../common.hpp"
+#include "model.hpp"
 
-// #include <triqs/utility/first_include.hpp>
-// // #include <triqs/utility/variant.hpp>
-// #include <vector>
+#include <triqs/arrays/math_functions.hpp>
+#include <triqs/utility/first_include.hpp>
+// #include <triqs/utility/variant.hpp>
+#include <vector>
 
-// namespace keldy::impurity_oneband {
+namespace keldy::impurity_oneband {
 
-// using namespace triqs::arrays;
-// using namespace triqs::gfs;
+using namespace triqs::arrays;
+using namespace triqs::gfs;
 
-// // Small wrapper for data
-// class sparse_kernel {
-// };
+// Small wrapper for data
+class sparse_kernel_binner {
+ public:
+  std::vector<std::pair<gf_index_t, dcomplex>> data{};
 
-// /// Kernel binner for Green Function $K(Y, X')$ with binning happening over $Y$
-// /// and $X'$ fixed by boundary conditions.
-// ///
-// /// TODO: How to include spin up / down separatley.
-// ///
-// class kernel_binner {
-//   double t_min = 0.0;
-//   double t_max = 1.0;
-//   int nr_bins = 100;
-//   double bin_size = 0.01;
+  double sum_weights() {
+    double result = 0.0;
+    for (const auto &rh : data) {
+      result += std::abs(rh.second);
+    }
+    return result;
+  }
 
-//   // // External point $X'$
-//   // gf_index_t external_point_X;
+  // make funcitons explicit for wrapping
+  sparse_kernel_binner &operator*=(double scalar) {
+    for (auto &rh : data) {
+      rh.second *= scalar;
+    }
+    return *this;
+  }
 
-//   int nr_point_dropped = 0;
+  sparse_kernel_binner &operator*=(dcomplex scalar) {
+    for (auto &rh : data) {
+      rh.second *= scalar;
+    }
+    return *this;
+  }
+};
 
-//   array<dcomplex, 2> values;    // bin (time), keldysh index, orbitals
-//   array<uint64_t, 2> nr_values; // bin (time), keldysh index, orbitals
-//   array<double, 1> bin_times;
+/// Kernel binner for Green Function $K(Y, X')$ with binning happening over $Y$
+/// and $X'$ fixed by boundary conditions.
+///
+/// TODO: How to include spin up / down separatley.
+class kernel_binner {
+  double t_min = 0.0;
+  double t_max = 1.0;
+  int nr_bins = 100;
+  double bin_size = 0.01;
 
-//  public:
-//   kernel_binner() = default;
-//   kernel_binner(double t_min_, double t_max_, int nr_bins_, gf_index_t external_point_X_)
-//      : t_min(t_min_),
-//        t_max(t_max_),
-//        nr_bins(nr_bins_),
-//       //  external_point_X(std::move(external_point_X_)),
-//        values(nr_bins, 2),
-//        nr_values(nr_bins, 2),
-//        bin_times(nr_bins) {
+  // // External point $X'$
+  // gf_index_t external_point_X;
 
-//     values() = 0;
-//     nr_values() = 0;
+  int nr_point_dropped = 0;
 
-//     // make checks?
-//     bin_size = (t_max - t_min) / nr_bins;
+  array<dcomplex, 2> values;    // bin (time), keldysh index, orbitals
+  array<uint64_t, 2> nr_values; // bin (time), keldysh index, orbitals
+  array<double, 1> bin_times;
 
-//     for (int i = 0; i < nr_bins; ++i) {
-//       bin_times(i) = t_min + (i + 0.5) * bin_size;
-//     }
-//   }
+ public:
+  kernel_binner() = default;
+  kernel_binner(double t_min_, double t_max_, int nr_bins_)
+     : t_min(t_min_),
+       t_max(t_max_),
+       nr_bins(nr_bins_),
+       //  external_point_X(std::move(external_point_X_)),
+       values(nr_bins_, 2),
+       nr_values(nr_bins_, 2),
+       bin_times(nr_bins_) {
 
-//   auto get_values() const { return values; }   
-//   auto get_nr_values() const { return nr_values; }
-//   auto get_bin_times() const { return bin_times; }
-//   auto get_bin_size() const { return bin_size; }
-//   auto get_nr_point_dropped() const { return nr_point_dropped; }
+    values() = 0;
+    nr_values() = 0;
 
-//   /// Includes boundary points, so t_min <= t <= t_max. t_max gets put in last bin
-//   void accumulate(gf_index_t const &a, dcomplex value) {
-//     if (t_min < a.time && a.time < t_max) {
-//       int bin = int((a.time - t_min) / bin_size);
-//       values(bin, a.k_idx) += value;
-//       nr_values(bin, a.k_idx)++;
-//     } else if (a.time == t_max) {
-//       values(nr_bins - 1, a.k_idx) += value;
-//       nr_values(nr_bins - 1, a.k_idx)++;
-//     } else {
-//       nr_point_dropped++;
-//     }
-//   }
+    // make checks?
+    bin_size = (t_max - t_min) / nr_bins;
 
-//   kernel_binner& operator+=(std::vector<std::pair<gf_index_t, dcomplex>> const & rhs){
-//     for(int i = 0; i < rhs.size(); i++){
-//       accumulate(rhs[i].first, rhs[i].second);
-//     }
-//     return *this;
-//   }
+    for (int i = 0; i < nr_bins; ++i) {
+      bin_times(i) = t_min + (i + 0.5) * bin_size;
+    }
+  }
 
-// };
+  auto get_values() const { return values; }
+  auto get_nr_values() const { return nr_values; }
+  auto get_bin_times() const { return bin_times; }
+  auto get_bin_size() const { return bin_size; }
+  auto get_nr_point_dropped() const { return nr_point_dropped; }
 
-// // implement this: double * std::vector<std::pair<gf_index_t, dcomplex>>
+  /// Includes boundary points, so t_min <= t <= t_max. t_max gets put in last bin
+  void accumulate(gf_index_t const &a, dcomplex value) {
+    if (t_min < a.time && a.time < t_max) {
+      int bin = int((a.time - t_min) / bin_size);
+      values(bin, a.k_idx) += value;
+      nr_values(bin, a.k_idx)++;
+    } else if (a.time == t_max) {
+      values(nr_bins - 1, a.k_idx) += value;
+      nr_values(nr_bins - 1, a.k_idx)++;
+    } else {
+      nr_point_dropped++;
+    }
+  }
 
+kernel_binner &operator+=(sparse_kernel_binner const &rhs) {
+  for (const auto &rh : rhs.data) {
+    accumulate(rh.first, rh.second);
+  }
+  return *this;
+}
 
-
-// class integrand_g_kernel {
-//   g0_keldysh_contour_t g0;
-//   gf_index_t g_idx_X; // Fixed Point in Kernal
-
-//   // bool expand_col = true; // expand_row = false
-//   // double condition_numebr_tol;
-
-//  public:
-//   /// Returns integrand for the specified times
-//   std::vector<std::pair<gf_index_t, dcomplex>> operator()(std::vector<double> const &times) const;
-
-//   integrand_g_kernel(g0_keldysh_contour_t g0_, gf_index_t g_idx_X_) : g0(std::move(g0_)), g_idx_X(std::move(g_idx_X_)) {};
-// };
-
-// // template<kernel_binner>
-// // constexpr bool is_binned_variable = true;
+};
 
 
-// } // namespace keldy::impurity_oneband
 
+class integrand_g_kernel {
+  g0_keldysh_contour_t g0;
+  gf_index_t g_idx_X; // Fixed Point in Kernal
+
+  // bool expand_col = true; // expand_row = false
+  // double condition_numebr_tol;
+
+ public:
+  /// Returns integrand for the specified times
+  using result_t = sparse_kernel_binner;
+  result_t operator()(std::vector<double> const &times) const;
+
+  integrand_g_kernel(g0_keldysh_contour_t g0_, gf_index_t g_idx_X_)
+     : g0(std::move(g0_)), g_idx_X(std::move(g_idx_X_)){};
+};
+
+// template<kernel_binner>
+// constexpr bool is_binned_variable = true;
+
+} // namespace keldy::impurity_oneband
