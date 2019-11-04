57a37268c36c71540dd2268cb334da27b4330932
Corentin Bertrand <cbertrand@flatironinstitute.org>
2019-11-04 17:11:29 -0500

    Test kernel order 2

--- test/c++/impurity_oneband/wick_kernel_test.cpp
+++ test/c++/impurity_oneband/wick_kernel_test.cpp
@@ -35,6 +35,58 @@ TEST(integrand_kernel, Order_1) { // NOLINT
   }
 }
 
+TEST(integrand_kernel, Order_2) { // NOLINT
+  model_param_t const params;
+  g0_model const g0{params};
+  g0_keldysh_contour_t const g0_k{g0};
+  auto const external_B = gf_index_t{5.0, up, forward};
+  integrand_g_kernel const integrand{g0_k, external_B};
+
+  double const u = 2.0;
+  double const v = 4.0;
+
+  auto const det_down = [&g0_k, u, v](keldysh_idx_t a, keldysh_idx_t b) -> dcomplex {
+    return g0_k({u, down, forward}, {u, down, backward}) * g0_k({v, down, forward}, {v, down, backward})
+       - g0_k({v, down, b}, {u, down, a}) * g0_k({u, down, a}, {v, down, b});
+  };
+
+  auto const det_prod_a = [&g0_k, &det_down, u, v, external_B](keldysh_idx_t a, keldysh_idx_t b) -> dcomplex {
+    int const sign = (a == b) ? +1 : -1;
+    return sign
+       * (g0_k({u, up, a}, {v, up, b}) * g0_k({v, up, b}, external_B) * det_down(a, b)
+          + g0_k({v, up, forward}, {v, up, backward}) * g0_k({u, up, a}, external_B) * g0_k({v, down, b}, {u, down, a})
+             * g0_k({u, down, a}, {v, down, b}));
+  };
+
+  auto const det_prod_b = [&g0_k, &det_down, u, v, external_B](keldysh_idx_t a, keldysh_idx_t b) -> dcomplex {
+    int const sign = (a == b) ? +1 : -1;
+    return sign
+       * (-g0_k({v, up, b}, {u, up, a}) * g0_k({u, up, a}, external_B) * det_down(a, b)
+          - g0_k({u, up, forward}, {u, up, backward}) * g0_k({v, up, b}, external_B) * g0_k({v, down, b}, {u, down, a})
+             * g0_k({u, down, a}, {v, down, b}));
+  };
+
+  auto expected_val =
+     sparse_kernel_binner{{{{u, up, forward}, det_prod_a(forward, forward) + det_prod_a(forward, backward)},
+                           {{u, up, backward}, det_prod_a(backward, forward) + det_prod_a(backward, backward)},
+                           {{v, up, forward}, det_prod_b(forward, forward) + det_prod_a(backward, forward)},
+                           {{v, up, backward}, det_prod_b(forward, backward) + det_prod_a(backward, backward)}}}
+        .data;
+
+  auto computed_val = integrand(std::vector<double>{u, v}).data;
+  ASSERT_EQ(computed_val.size(), 4);
+
+  auto const compare = [](std::pair<gf_index_t, dcomplex> const &a, std::pair<gf_index_t, dcomplex> const &b) -> bool {
+    return a.first < b.first;
+  };
+  std::sort(expected_val.begin(), expected_val.end(), compare);
+  std::sort(computed_val.begin(), computed_val.end(), compare);
+
+  for (int i = 0; i < 4; ++i) {
+    EXPECT_COMPLEX_NEAR(computed_val[i].second, expected_val[i].second, 1e-16);
+  }
+}
+
 TEST(ComputeObs, Initialize2) { // NOLINT
   model_param_t params;
   compute_gf_kernel computer(params, 10.0, 4, "identity", 1000);
