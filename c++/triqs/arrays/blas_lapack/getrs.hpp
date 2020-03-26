b1ea2f4f682309703dc099f71338a5e25110e548
Philipp Dumitrescu <pdumitrescu@flatironinstitute.org>
2020-03-26 15:06:13 -0400

    update banner in files & main license file

--- c++/triqs/arrays/blas_lapack/getrs.hpp
+++ c++/triqs/arrays/blas_lapack/getrs.hpp
@@ -1,10 +1,9 @@
 /*******************************************************************************
  *
- * TRIQS: a Toolbox for Research in Interacting Quantum Systems
+ * TRIQS: A Toolbox for Research in Interacting Quantum Systems
  *
- * Copyright (C) 2012-2017 by O. Parcollet
- * Copyright (C) 2018 by Simons Foundation
- *   author : O. Parcollet, P. Dumitrescu, N. Wentzell
+ * Copyright (c) 2019 The Simons Foundation
+ *   authors: Philipp Dumitrescu
  *
  * TRIQS is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
@@ -31,23 +30,27 @@
 
 namespace triqs::arrays::lapack {
 
-  using namespace blas_lapack_tools;
+using namespace blas_lapack_tools;
 
-  /**
+/**
   * Calls getrs on a matrix or view
   * Takes care of making temporary copies if necessary
   */
-  template <typename MT>
-  typename std::enable_if<is_blas_lapack_type<typename MT::value_type>::value, int>::type getrs(MT &A, MT &B, triqs::arrays::vector<int> &ipiv) {
-    // assert(A.memory_layout_is_fortran() && B.memory_layout_is_fortran());  // For now enforce this
-    if (A.memory_layout_is_c() || B.memory_layout_is_c()) TRIQS_RUNTIME_ERROR << "matrices passed to getrs is not in Fortran order";
+template <typename MT>
+typename std::enable_if<is_blas_lapack_type<typename MT::value_type>::value, int>::type
+getrs(MT &A, MT &B, triqs::arrays::vector<int> &ipiv) {
+  // assert(A.memory_layout_is_fortran() && B.memory_layout_is_fortran());  // For now enforce this
+  if (A.memory_layout_is_c() || B.memory_layout_is_c())
+    TRIQS_RUNTIME_ERROR << "matrices passed to getrs is not in Fortran order";
 
-    reflexive_qcache<MT> Ca(A);
-    auto dm = first_dim(Ca());
-    if (ipiv.size() != dm) TRIQS_RUNTIME_ERROR << "getrs : error in ipiv size : found " << ipiv.size() << " while it should be " << dm;
-    reflexive_qcache<MT> Cb(B);
-    int info;
-    triqs::arrays::lapack::f77::getrs('N', dm, get_n_cols(Cb()), Ca().data_start(), get_ld(Ca()), ipiv.data_start(), Cb().data_start(), get_ld(Cb()), info);
-    return info;
-  }
+  reflexive_qcache<MT> Ca(A);
+  auto dm = first_dim(Ca());
+  if (ipiv.size() != dm)
+    TRIQS_RUNTIME_ERROR << "getrs : error in ipiv size : found " << ipiv.size() << " while it should be " << dm;
+  reflexive_qcache<MT> Cb(B);
+  int info;
+  triqs::arrays::lapack::f77::getrs('N', dm, get_n_cols(Cb()), Ca().data_start(), get_ld(Ca()), ipiv.data_start(),
+                                    Cb().data_start(), get_ld(Cb()), info);
+  return info;
+}
 } // namespace triqs::arrays::lapack
