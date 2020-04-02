649ff62ea2b8c626a62eba7a19e9b04c7f253c0e
Philipp Dumitrescu <pdumitrescu@flatironinstitute.org>
2020-04-02 15:53:48 -0400

    update license banners

--- python/keldy/__init__.py
+++ python/keldy/__init__.py
@@ -1,26 +1,24 @@
-################################################################################
+###############################################################################
 #
-# TRIQS: a Toolbox for Research in Interacting Quantum Systems
+# keldy
 #
-# Copyright (C) 2016-2018, N. Wentzell
-# Copyright (C) 2018-2019, The Simons Foundation
-#   author: N. Wentzell
+# Copyright (c) 2019-2020 The Simons Foundation
+#   authors: Philipp Dumitrescu
 #
-# TRIQS is free software: you can redistribute it and/or modify it under the
+# keldy is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# TRIQS is distributed in the hope that it will be useful, but WITHOUT ANY
+# keldy is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along with
-# TRIQS. If not, see <http://www.gnu.org/licenses/>.
+# keldy. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
-
+##############################################################################/
 r"""
 DOC
 
@@ -28,19 +26,24 @@ DOC
 
 from cpp2py import Cpp2pyInfoBase
 
+
 class Cpp2pyInfo(Cpp2pyInfoBase):
 
     table_imports = {
-        'keldy' : 'keldy.common',
-        'keldy::warpers' : 'keldy.warpers',
-        'keldy::impurity_oneband' : 'keldy.impurity_oneband',
-        }
+        'keldy': 'keldy.common',
+        'keldy::warpers': 'keldy.warpers',
+        'keldy::impurity_oneband': 'keldy.impurity_oneband',
+    }
 
     # _table_converters = {}
 
-    table_converters = dict() # (k, "triqs/cpp2py_converters/%s.hpp"%v) for (k,v) in _table_converters.items())
+    table_converters = dict(
+    )  # (k, "triqs/cpp2py_converters/%s.hpp"%v) for (k,v) in _table_converters.items())
+
 
 # def _get_cpp2py_wrapped_class_enums():
 #     return {'module_name' : 'UNUSED', 'includes' : "['<triqs/cpp2py_converters.hpp>']"}
 
-__all__ = ['Cpp2pyInfo', 'common', 'warpers', 'impurity_oneband', 'visualization']
+__all__ = [
+    'Cpp2pyInfo', 'common', 'warpers', 'impurity_oneband', 'visualization'
+]
