5477970b5e3ff1817919f54b99510ac6bd83d288
Corentin Bertrand <cbertrand@flatironinstitute.org>
2020-05-26 12:18:30 -0400

    [binner] remove cpp

--- c++/keldy/binner.hpp
+++ c++/keldy/binner.hpp
@@ -121,15 +121,22 @@ struct continuous_axis_t {
   double bin_size;
 };
 
-void mpi_broadcast(continuous_axis_t &in, mpi::communicator c = {}, int root = 0);
+inline void mpi_broadcast(continuous_axis_t &in, mpi::communicator c, int root) {
+  mpi::broadcast(in.xmin, c, root);
+  mpi::broadcast(in.xmax, c, root);
+  mpi::broadcast(in.nr_bins, c, root);
+  mpi::broadcast(in.bin_size, c, root);
+};
 
-bool operator==(continuous_axis_t const &a, continuous_axis_t const &b);
+inline bool operator==(continuous_axis_t const &a, continuous_axis_t const &b) {
+  return (a.xmin == b.xmin) && (a.xmax == b.xmax) && (a.nr_bins == b.nr_bins) && (a.bin_size == b.bin_size);
+};
 
 struct discreet_axis_t {
   long nr_bins;
 };
 
-void mpi_broadcast(discreet_axis_t &in, mpi::communicator c = {}, int root = 0);
+inline void mpi_broadcast(discreet_axis_t &in, mpi::communicator c, int root) { mpi::broadcast(in.nr_bins, c, root); };
 
 /*
  * Multidimentional binner with N continuous coordinates and M discreet ones.
